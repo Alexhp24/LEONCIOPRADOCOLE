577d83660031043784264945abcb74cdcb98993f
Alexhp24 <alexhidalgopalma20@gmail.com>
2024-12-03 22:09:51 -0500

    contactos completo

--- vistas/js/script.js
+++ vistas/js/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var myCarousel = document.querySelector('#carouselExample');
-  var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 4000 // Tiempo en milisegundos (4 segundos)
-  });
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   var myCarousel = document.querySelector('#carouselExample');
+//   var carousel = new bootstrap.Carousel(myCarousel, {
+//     interval: 4000 // Tiempo en milisegundos (4 segundos)
+//   });
+// });
